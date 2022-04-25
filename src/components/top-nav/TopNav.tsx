6102b1d244f220db6f0395a1072bbee0358153b8
Hinrich <johnhinrichggalindo@gmail.com>
2022-04-25 15:54:14 +0800

    seperated the style for navbar list and navbar icon

--- src/components/top-nav/TopNav.tsx
+++ src/components/top-nav/TopNav.tsx
@@ -1,19 +1,25 @@
 import React from 'react'
+import { IconType } from 'react-icons';
 import { AiOutlineHome, AiOutlineMail} from 'react-icons/ai';
 import { BsPersonCircle } from 'react-icons/bs';
 
-export default function TopNav() {
+interface TopNavProps {
+  className?: string,
+  icon?: IconType
+}
+
+export default function TopNav ({className}:TopNavProps) {
   return (
-    <nav className=' bg-emerald-500'>
+    <nav className='bg-emerald-500' >
       <ul className='flex ml-auto mr-4 w-fit'>
-        <li className='navbar-icon'> 
-        <AiOutlineHome size="1.5em" title="Home"/> 
+        <li className='navbar-list'> 
+        <AiOutlineHome size="1.5em" title="Home" className='navbar-icon'/> 
         </li>
-        <li className='navbar-icon'>
-          <BsPersonCircle size="1.5em" title="About"/>
+        <li className='navbar-list'>
+          <BsPersonCircle size="1.5em" title="About" className='navbar-icon'/>
           </li>
-        <li className='navbar-icon'>
-          <AiOutlineMail size="1.5em" title="Contact"/>
+        <li className='navbar-list'>
+          <AiOutlineMail size="1.5em" title="Contact" className='navbar-icon'/>
         </li>
       </ul>
     </nav>
