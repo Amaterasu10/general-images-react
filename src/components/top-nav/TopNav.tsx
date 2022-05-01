d28c0d8392f6965307164c9b37ca1b675ef47cde
Hinrich <johnhinrichggalindo@gmail.com>
2022-05-02 00:01:23 +0800

    removed the nav icons and replaced it with text instead

--- src/components/top-nav/TopNav.tsx
+++ src/components/top-nav/TopNav.tsx
@@ -1,8 +1,4 @@
 import React from 'react'
-import { IconType } from 'react-icons';
-import { AiOutlineHome, AiOutlineMail} from 'react-icons/ai';
-import { BsPersonCircle } from 'react-icons/bs';
-
 import { Link } from 'react-router-dom'
 
 interface TopNavProps {
@@ -16,18 +12,18 @@ export default function TopNav ({className}:TopNavProps) {
       <ul className='flex ml-auto mr-4 w-fit'>
         <li className='navbar-list'> 
           <Link to="/">
-            <AiOutlineHome size="1.5em" title="Home" className='navbar-icon'/>
+            Home
           </Link>
         </li>
 
         <li className='navbar-list'>
           <Link to="contact">
-            <AiOutlineMail size="1.5em" title="Contact" className='navbar-icon'/>
+            Contact
           </Link>
         </li>
         <li className='navbar-list'>
           <Link to="about">
-            <BsPersonCircle size="1.5em" title="About" className='navbar-icon'/>
+            About
           </Link>
         </li>
       </ul>
