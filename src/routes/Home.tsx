077be38a1f418a653234e82137a6e52ed1eaf40a
Hinrich <johnhinrichggalindo@gmail.com>
2022-05-01 23:49:42 +0800

    added the title of the website at Home.tsx and removing the unnecessary codes

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,36 +1,41 @@
 import React, { useState } from 'react'
 import { GoSearch } from 'react-icons/go';
-import { BsSearch } from 'react-icons/bs';
+import { useNavigate } from 'react-router-dom';
 
 
 export default function Home() {
-  const [search, setSearch] = useState(null);
-
+  const navigate = useNavigate();
+  const [searchInput, setSearchInput] = useState(null);
   const onChange = (e: React.FormEvent<HTMLInputElement>) => {
-    setSearch(e.currentTarget.value);
+    setSearchInput(e.currentTarget.value);
   }
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(event.target[0].value)
+    navigate(`/gallery/${searchInput}`);
   }
     
   return (
     <div id="home">
       <section className='grid place-items-center h-[calc(100vh-48px)] p-3'>
+        <div className='w-full grid place-items-center'>
+        <h1 className='text-3xl md:text-5xl '>General Images</h1>
         <form 
-          className='search-form rounded-full bg-slate-50 hover:bg-slate-100 shadow-inner'
+          action='gallery'
+          className='search-form rounded-full bg-slate-100 hover:bg-slate-100 shadow-inner mt-9'
+          onSubmit={handleSubmit}
         >
           <button type="submit" className='p-2'>
             <GoSearch size="1.2em"/>
           </button>
           <input 
-          type="text" 
-          name="search-input" 
-          className='w-full p-1 focus-visible:outline-0 bg-transparent'
-          onChange={(e)=>onChange(e)}
+            type="text" 
+            name="search-input" 
+            className='w-full p-3 focus-visible:outline-0 bg-transparent rounded-full focus-within:bg-slate-200'
+            onChange={(e)=>onChange(e)}
           />
         </form>
+        </div>
       </section>
     </div>
   )
