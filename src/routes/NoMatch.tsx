ea0c00827f3a619b71894cdc22c79030dfac2b53
Hinrich <johnhinrichggalindo@gmail.com>
2022-05-01 16:47:32 +0800

    added some text to noMatch route

--- src/routes/NoMatch.tsx
+++ src/routes/NoMatch.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function NoMatch() {
   return (
-    <div>NoMatch</div>
+    <section className='grid place-items-center h-[calc(100vh-48px)] p-3'>
+      <h2>The page that you are looking for does not exist -  
+      <span className='font-bold text-lg'> 404</span>
+      </h2>
+    </section>
   )
 }
