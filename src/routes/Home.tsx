f757c2b1ccb60cafd108fdd957dcfca0a1dc68b3
Hinrich <johnhinrichggalindo@gmail.com>
2022-05-04 14:45:20 +0800

    added random button for search and implemented some logic so that users cant click search when input is null

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { GoSearch } from 'react-icons/go';
+import { FaRandom } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
 
 
@@ -25,15 +26,24 @@ export default function Home() {
           className='search-form rounded-full bg-slate-100 hover:bg-slate-100 shadow-inner mt-9'
           onSubmit={handleSubmit}
         >
-          <button type="submit" className='p-2'>
-            <GoSearch size="1.2em"/>
-          </button>
+          { 
+            searchInput && 
+            <button type="submit" className='p-2'>
+              <GoSearch size="1.2em"/>
+            </button>
+          }
           <input 
             type="text" 
             name="search-input" 
-            className='w-full p-3 focus-visible:outline-0 bg-transparent rounded-full focus-within:bg-slate-200'
+            className='w-full p-3 focus-visible:outline-0 bg-transparent rounded-full focus-within:bg-slate-200 pl-5'
             onChange={(e)=>onChange(e)}
           />
+          {
+            
+            <button type="submit" className='p-2'>
+            <FaRandom size="1.2em"/>
+            </button>
+          }
         </form>
         </div>
       </section>
