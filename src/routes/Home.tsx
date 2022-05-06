d6fcafbf6d4994ef710d309febe4e2732df2d7bd
Hinrich <johnhinrichggalindo@gmail.com>
2022-05-06 13:20:01 +0800

    added focus-within effect for form

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -15,33 +15,33 @@ export default function Home() {
     event.preventDefault();
     navigate(`/gallery/${searchInput}`);
   }
-  console.log(import.meta.env.VITE_APP_TITLE)
+  console.log(import.meta.env.VITE_UNSPLASH_SECRET_KEY)
   return (
     <div id="home">
       <section className='landing-screen'>
         <div className='w-full grid place-items-center'>
-        <h1 className='text-3xl md:text-5xl '>General Images</h1>
-        <form 
-          action='gallery'
-          className='search-form rounded-full bg-slate-100 hover:bg-slate-100 shadow-inner mt-9'
-          onSubmit={handleSubmit}
-        >
-          { 
-            searchInput && 
-            <button type="submit" className='p-2'>
-              <GoSearch size="1.2em"/>
+          <h1 className='text-3xl md:text-5xl '>General Images</h1>
+          <form 
+            action='gallery'
+            className='search-form rounded-full bg-slate-100 hover:bg-slate-200 shadow-inner mt-9 focus-within:bg-slate-200'
+            onSubmit={handleSubmit}
+          >
+            { 
+              searchInput && 
+              <button type="submit" className='p-2 rounded-l-full'>
+                <GoSearch size="1.2em"/>
+              </button>
+            }
+            <input 
+              type="text" 
+              name="search-input" 
+              className='w-full p-3 focus-visible:outline-0 bg-transparent  pl-5 rounded-l-full'
+              onChange={(e)=>onChange(e)}
+            />
+            <button type="submit" className='p-2 rounded-r-full'>
+              <FaRandom size="1.2em"/>
             </button>
-          }
-          <input 
-            type="text" 
-            name="search-input" 
-            className='w-full p-3 focus-visible:outline-0 bg-transparent rounded-full focus-within:bg-slate-200 pl-5'
-            onChange={(e)=>onChange(e)}
-          />
-          <button type="submit" className='p-2'>
-            <FaRandom size="1.2em"/>
-          </button>
-        </form>
+          </form>
         </div>
       </section>
     </div>
