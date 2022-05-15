b3521fee39dcfc6d0ca094bdb1b27871e23e44ae
Hinrich <johnhinrichggalindo@gmail.com>
2022-05-15 19:55:19 +0800

    used react fragments to contains all the returned contents of gallery.tsx

--- src/routes/Gallery.tsx
+++ src/routes/Gallery.tsx
@@ -32,44 +32,46 @@ export default function Images() {
 
 
   return (
-    <section className='landing-screen'>
-      <div 
-      className={
-        images.length && `grid sm:grid-cols-masonry sm:grid-rows-masonry`
-      }>
-        {
-          isLoading && 
-          <p>Loading...</p>
-        }
-        { 
-          images.length > 0 &&
-          images.map( image => {
-            return (
-              <div 
-                key={image?.id} 
-                className={
-                  `shadow-2xl bg-slate-100 p-3 m-5 grid place-content-center place-items-center ${image.width > image.height ? "col-span-3 row-span-2" : "col-span-3 row-span-4"}`
-                }  
-              >
-                <img 
-                  src={image?.urls.small_s3} 
-                  alt={image?.alt_description}
-                  height={image?.height}
-                  width={image.width} 
-                  loading="lazy"
-                  className="max-w-full h-auto align-middle inline-block"
-                />
-                {/* <p>{image.alt_description}</p> */}
-              </div>
-            )
-          })
-        }
+    <>
+      <section className='landing-screen'>
+        <div 
+        className={
+          images.length && `grid sm:grid-cols-masonry sm:grid-rows-masonry`
+        }>
+          {
+            isLoading && 
+            <p>Loading...</p>
+          }
+          { 
+            images.length > 0 &&
+            images.map( image => {
+              return (
+                <div 
+                  key={image?.id} 
+                  className={
+                    `shadow-2xl bg-slate-100 p-3 m-5 grid place-content-center place-items-center ${image.width > image.height ? "col-span-3 row-span-2" : "col-span-3 row-span-4"}`
+                  }  
+                >
+                  <img 
+                    src={image?.urls.small_s3} 
+                    alt={image?.alt_description}
+                    height={image?.height}
+                    width={image.width} 
+                    loading="lazy"
+                    className="max-w-full h-auto align-middle inline-block"
+                  />
+                  {/* <p>{image.alt_description}</p> */}
+                </div>
+              )
+            })
+          }
 
-        { 
-          !isLoading && !images.length &&
-          <p className='text-center'>{`No search result for "${searchInput}`}</p>
-        }
-      </div>
-    </section>
+          { 
+            !isLoading && !images.length &&
+            <p className='text-center'>{`No search result for "${searchInput}`}</p>
+          }
+        </div>
+      </section>
+    </>
   )
 }
